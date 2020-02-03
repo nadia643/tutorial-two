bc911a862c0890df85ce0b27439c227e6db44b8e
John Doe <nadia643@gmail.com>
2020-02-03 14:46:54 +0000

    created person component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React, { component } from 'react';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const Person = ({img, name, age}) => {
+  return(
+    <article>
+      <img src={img} alt="person" />
+      <h4>name: {name}</h4>
+      <h4>age: {age}</h4>
+    </article>
+  )
+}
+
+class PersonList extends Component {
+  render() {
+    return (
+      <section>
+        <Person />
+      </section>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return <PersonList />
+  }
 }
 
 export default App;
